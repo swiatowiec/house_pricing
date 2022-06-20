11e3e41214ac637c21647300ccfda76ceb36c0e4
swiatowiec <katarzynaswiatowiec@gmail.com>
2022-06-20 19:31:27 +0200

    plot test - working

--- model/tests.py
+++ model/tests.py
@@ -2,6 +2,9 @@ import unittest
 from dataset_repository import DatasetRepository
 from model_service import ModelService
 from facade import PredictHousePrices
+import tempfile
+import uuid
+import os
 
 
 class ModelTests(unittest.TestCase):
@@ -17,3 +20,12 @@ class ModelTests(unittest.TestCase):
         train_predict = self.setUp()
         dataset, features, target = train_predict._dataset_repository.read_data()
         self.assertEqual(dataset.shape, (506, 14))
+
+    def test_if_plots_are_created(self):
+        PATH = os.path.join(tempfile.gettempdir(), str(uuid.uuid4()))
+        os.mkdir(PATH)
+        train_predict = self.setUp()
+        dataset, features, target = train_predict._dataset_repository.read_data()
+        train_predict._model_service.generate_plots(dataset, PATH)
+        num_of_creates_plots = len(os.listdir(PATH))
+        self.assertEqual(num_of_creates_plots, 2)
