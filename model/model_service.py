a862c895adfcad34ca52448d662b350bc288cf5d
swiatowiec <katarzynaswiatowiec@gmail.com>
2022-06-20 18:08:50 +0200

    train on selected features

--- model/model_service.py
+++ model/model_service.py
@@ -3,6 +3,8 @@ from sklearn.linear_model import LinearRegression
 import matplotlib.pyplot as plt
 import seaborn as sns
 import os
+import pandas as pd
+import numpy as np
 
 class ModelService:
     def generate_plots(self, dataset):
@@ -16,8 +18,14 @@ class ModelService:
         plt.savefig('prices_(MEDV).png')
 
     def train_predict(self, features, target):
+        selected_features = self._select_features(features=features)
         #TODO test size as params
-        X_train, X_test, y_train, y_test = train_test_split(features, target, test_size=0.2, random_state = 42)
+        X_train, X_test, y_train, y_test = train_test_split(selected_features, target, test_size=0.2, random_state = 42)
         reg = LinearRegression()
         reg.fit(X_train, y_train)
         pred = reg.predict(X_test)
+
+    def _select_features(self, features):
+        #feature selection based on corr matrix
+        selected_features = pd.DataFrame(np.c_[features['RM'], features['INDUS'], features['NOX'], features['TAX'], features['PTRATIO'], features['LSTAT']])
+        return selected_features   
